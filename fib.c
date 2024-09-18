0a7adb8081608b8a4408306202488b6cf2c55b2e
BXM22 <brennen@csuf.fullerton.edu>
2024-09-18 08:49:08 -0700

    large_fib

--- fib.c
+++ fib.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
-#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char* argv[]) {
-   printf("Hello!\n");
+unsigned long long recursive_fib(unsigned long long n) {
+    if (n == 1) return 0;
+    if (n == 2) return 1;
+      return recursive_fib(n - 1) + recursive_fib(n - 2);
+}
+unsigned long long iterative_fib(unsigned long long int n) {
+    if (n == 1) return 0; //shifts the n back by 1
+    if (n == 2) return 1;
+    int a = 0, b = 1, c;
+    for (int i = 3; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
+int main(int argc, char *argv[]) { 
+   if (argc != 3) {  
+      printf("Usage: %s <integer> <r|i>\n", argv[0]); 
+      return 1;
+   }
+   //Get the arguments
+   unsigned int long long num1 = atoi(argv[1]);
+   char mode = argv[2][0];
+   
+   unsigned long long int fib = num1;
+
+   //determine modes
+   if (mode == 'r') {
+      fib = recursive_fib(fib);
+   } else if (mode == 'i') {
+      fib = iterative_fib(fib);
+   } else {
+      printf("Error: Invalid mode\n");
+      return 1;
+   }
+   printf("%llu", fib); //Result
    return 0;
 }
